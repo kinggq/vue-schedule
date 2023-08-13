481c23f51e98b077a58280821a67719c60685887
kinggq <18701094030@163.com>
2023-08-13 19:18:31 +0800

    chore: update

--- packages/schedule.vue
+++ packages/schedule.vue
@@ -1,65 +1,104 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
+const tbodyRef = ref<HTMLElement>()
 const weeks = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
 
 const isDragging = ref(false)
-const startCell = ref<number>()
-const endCell = ref<number>()
 const left = ref<number>()
 const top = ref<number>()
-const clientX = ref<number>()
-const clientY = ref<number>()
+const startX = ref<number>()
+const startY = ref<number>()
 const width = ref(0)
 const height = ref(0)
 
-function startDrag(event: MouseEvent, cellIndex: number) {
-  // console.log(event)
-  clientX.value = event.clientX
-  left.value = event.clientX
-  top.value = event.clientY
-  clientY.value = event.clientY
-  isDragging.value = true
-  startCell.value = cellIndex
-  endCell.value = cellIndex
+const tbodyTop = ref<number>(0)
+const tbodyLeft = ref<number>(0)
+const tbodyWidth = ref<number>(0)
+const tbodyHeight = ref<number>(0)
+
+function startDrag(event: MouseEvent) {
+  if (tbodyRef.value) {
+    const rect = tbodyRef.value.getBoundingClientRect()
+    tbodyTop.value = rect.top + window.scrollY
+    tbodyLeft.value = rect.left + window.scrollX + 80
+    tbodyWidth.value = tbodyRef.value.clientWidth
+    tbodyHeight.value = tbodyRef.value.clientHeight
+    // const cell = tbodyHeight.value / 7
+    // const row = tbodyWidth.value / 48
+    // console.log('row', row)
+    // console.log('top', top, ',left', left, ',tbodyWidth', tbodyWidth)
+    if (handleBeyond(event.clientX, event.clientY)) {
+      // console.log(((event.clientX - tbodyLeft.value) / row))
+      startX.value = event.clientX
+      startY.value = event.clientY
+      left.value = event.clientX
+      top.value = event.clientY
+      isDragging.value = true
+    }
+    document.addEventListener('mouseup', stopDrag)
+  }
 }
 
-function handleDrag(event: MouseEvent, cellIndex: number) {
+function handleDrag(event: MouseEvent) {
   if (isDragging.value) {
-    width.value = Math.abs(event.clientX - clientX.value!)
-    height.value = Math.abs(event.clientY - clientY.value!)
-    endCell.value = cellIndex
-    if (event.clientX - left.value! < 0)
+    if (!handleBeyond(event.clientX, event.clientY))
+      return
+
+    width.value = Math.abs(event.clientX - startX.value!)
+    height.value = Math.abs(event.clientY - startY.value!)
+    if (event.clientX < startX.value!)
       left.value = event.clientX
 
-    if (event.clientY - top.value! < 0)
+    if (event.clientY < startY.value!)
       top.value = event.clientY
   }
 }
 
 function stopDrag() {
   isDragging.value = false
+
+  document.removeEventListener('mouseup', stopDrag)
 }
 
 function getMaskPosition() {
   return `left: ${left.value}px; top:${top.value}px;width:${width.value}px;height:${height.value}px;`
+}
+
+function handleBeyond(x: number, y: number) {
+  if (x > tbodyLeft.value && x < tbodyLeft.value + tbodyWidth.value && y > tbodyTop.value && y < tbodyHeight.value + tbodyTop.value)
+    return true
+  return false
+}
+
+watch(() => isDragging.value, (newValue) => {
+  if (!newValue)
+    resetStatus()
+})
+
+function resetStatus() {
+  left.value = undefined
+  top.value = undefined
+  startX.value = undefined
+  startY.value = undefined
+  width.value = 0
+  height.value = 0
 }
 </script>
 
 <template>
   <pre>
     isDragging:{{ isDragging }}
-  startCell:{{ startCell }}
-  endCell:{{ endCell }}
-  clientX:{{ clientX }}
-  clientY:{{ clientY }}
+  startX:{{ startX }}
+  startY:{{ startY }}
   width: {{ width }}
   height: {{ height }}
+  left: {{ left }}
+  top: {{ top }}
   </pre>
   <div
-    h-600px w-800px select-none border rounded-4px @mousedown="startDrag($event, 1)"
-    @mousemove="handleDrag($event, 1)"
-    @mouseup="stopDrag"
+    h-600px w-800px select-none border rounded-4px @mousedown="startDrag"
+    @mousemove="handleDrag"
   >
     <div h-40px flex items-center px-20px border-b="1px">
       <div flex="~ gap3" text="12px">
@@ -103,7 +142,7 @@ function getMaskPosition() {
             </th>
           </tr>
         </thead>
-        <tbody>
+        <tbody ref="tbodyRef">
           <tr
             v-for="(week, index) in weeks"
             :key="index"
